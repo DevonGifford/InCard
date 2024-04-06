87b954f5f5b915ec22945955c51d1a89be5cc132
Devon Gifford <118319299+DevonGifford@users.noreply.github.com>
2024-04-07 00:54:40 +0200

    feat: add scrolling logo animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,75 @@
-import Link from "next/link";
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 import Button from "../components/ui/button";
-import splashImage from "@/public/incard-images/home_horizontal_card.png"; 
-import clientLogos from "@/public/client-logos";  
+
+import clientLogos from "@/public/client-logos";
 
 const clientLogo: { [key: string]: StaticImageData } = clientLogos;
 
-export default async function Home() {  
+export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between m-5 sm:m-10">
-      <div className="flex flex-col justify-center gap-3 sm:gap-7 items-center text-center pt-5 sm:pt-20">
-        <header className="text-center pb-2 font-bold text-4xl lg:text-6xl mx-10">
+    <main className="flex flex-col h-[80vh] justify-evenly items-center  gap-8 bg-cover bg-center bg-no-repeat bg-[url('/incard-images/home_horizontal_card.png')] ">
+      <header className="flex flex-col text-center space-y-4 md:space-y-14 ">
+        <p className="flex flex-col font-bold text-4xl lg:text-6xl">
           <span className="text-incard-blue">Grow your online </span>
           business <br /> with incard.
-        </header>
-
-        <span className=" max-w-2xl text-sm sm:text-base sm:mx-14">
-          This is a Frontend take home assessment. Consists of 3 pages. Utilizing SSR &
-          CSR. Authentication with persistent sessions and protected routes
-        </span>
+          <span className="max-w-2xl text-sm sm:text-base sm:mx-14 font-thin pt-6 text-white">
+            This is a Frontend take home assessment. Consists of 3 pages.
+            Utilizing SSR & CSR. Authentication with persistent sessions and
+            protected routes
+          </span>
+        </p>
 
         <Link href="/dashboard" className="text-sm">
           <Button text="Getting  Started" />
         </Link>
+      </header>
 
+      <footer className="flex flex-col justify-center items-center text-center gap-4 lg:gap-8">
+        <span className="font-semibold italic">
+          We work across all major ecom platforms
+        </span>
 
-        <Image
-          className="w-full -z-10 md:-translate-y-24"
-          src={splashImage}
-          width={900}
-          height={300}
-          alt="premium visa card"
-        />
-
-        <footer className="flex flex-col -translate-y-20 md:-translate-y-72 justify-center items-center text-center gap-2 md:gap-10">
-          <span>We work across all major ecom platforms</span>
-          <div className="flex flex-col justify-center items-center md:flex-row gap-6 ">
+        <div className="w-[90vw] md:w-[80vw] inline-flex flex-nowrap gap-20 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)] group">
+          <ul className="flex space-x-20 items-center justify-center animate-infinite-scroll group-hover:paused cursor-pointer">
             {Object.keys(clientLogos).map((iconName: string, index: number) => (
-              <div key={index}>
+              <div
+                key={index}
+                className="hidden lg:flex items-center justify-center animate-infinite-scroll group-hover:paused cursor-pointer"
+              >
                 <Image
                   src={clientLogo[iconName]}
-                  width={100}
+                  width={200}
                   height={200}
                   alt="company logo"
+                  className="max-w-none"
                 />
               </div>
             ))}
-          </div>
-        </footer>
-      </div>
+          </ul>
+
+          <ul
+            className="flex space-x-20 items-center justify-center animate-infinite-scroll group-hover:paused"
+            aria-hidden="true"
+          >
+            {Object.keys(clientLogos).map((iconName: string, index: number) => (
+              <div
+                key={index}
+                className="hidden lg:flex items-center justify-center  animate-infinite-scroll group-hover:paused cursor-pointer"
+                aria-hidden="true"
+              >
+                <Image
+                  src={clientLogo[iconName]}
+                  width={200}
+                  height={200}
+                  alt="company logo"
+                  className="max-w-none"
+                />
+              </div>
+            ))}
+          </ul>
+        </div>
+      </footer>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -17,13 +17,13 @@ module.exports = {
     },
     extend: {
       colors: {
-        'incard-blue': '#00cfcf',
+        "incard-blue": "#00cfcf",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        
+
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
@@ -53,7 +53,16 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      animation: {
+        "infinite-scroll": "infinite-scroll 97s linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
         "accordion-down": {
           from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -63,11 +72,7 @@ module.exports = {
           to: { height: 0 },
         },
       },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
